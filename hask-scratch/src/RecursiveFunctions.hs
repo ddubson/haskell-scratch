3e0cae2b43dbb6facf3435959321ad1601740ba8
Dmitriy Dubson <d.dubson@gmail.com>
2020-01-16 15:03:05 -0500

    Add more recursive function examples

--- hask-scratch/src/RecursiveFunctions.hs
+++ hask-scratch/src/RecursiveFunctions.hs
@@ -6,7 +6,28 @@ maximum' [] = error "max of empty list!"
 maximum' [x] = x
 maximum' (x:xs) = max x (maximum' xs)
 
+-- Determine if an entity in a list exists
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+  | a == x = True
+  | otherwise = a `elem'` xs
+
+-- Quicksort
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let smallerOrEqual = [a | a <- xs, a <= x]
+      larger = [a | a <- xs, a > x]
+  in quicksort smallerOrEqual ++ [x] ++ quicksort larger
+
 recFuncExamples :: IO ()
 recFuncExamples = do
   -- feed a list to maximum' fn and then feed that result to print
   print . maximum' $ [5,25,32,3]
+
+  -- feed a list to elem' fn with a member to find, then print the result
+  print $ elem' 12 [4,3,2,6,4,12,3,5]
+
+  -- Quicksort
+  print . quicksort $ [5,6,2,7,12,8,234,8723,233,2,42,6]
